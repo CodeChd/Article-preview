24aeedf959c662d113ff681450d4403c308c4bf3
CodeChd <Drowzyking1126@gmail.com>
2023-04-23 10:26:42 +0800

    Ready for Deploy

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
-const icon = document.querySelector(".hover-icon")
+const icon = document.querySelector(".enable")
+const svg = document.querySelector(".svg-toggle")
+const btn = document.querySelector(".icon-holder")
 
-const data = icon.getAttribute("data-visible")
+btn.getAttribute("data-visible")
+
+btn.addEventListener("click", (e) => {
+    
+    if(icon.classList.contains("enable")){
+        btn.setAttribute("data-visible", true)
+        svg.classList.remove("svg-toggle")
+        icon.classList.remove("enable")
+        bg.classList.remove()
+    }
+    
+    else{
+        btn.setAttribute("data-visible", false)
+        svg.classList.add("svg-toggle")
+        icon.classList.add("enable")
+    }
+
+      
+  
+    
+})
+
 
